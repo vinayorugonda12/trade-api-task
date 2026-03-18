fc7864afbed39f146f5645598ddf0f702bb66a0a
vinay orugonda <vinayorugonda15@gmail.com>
2026-03-18 12:22:09 +0530

    api reset

--- app/utils/auth.py
+++ app/utils/auth.py
@@ -1,6 +1,10 @@
+import os
 from fastapi import Header, HTTPException
+from dotenv import load_dotenv
 
-API_KEY = "AIzaSyDZ4mG2YTkodoLlt-FYkQN-h4UcQEAK98w"
+load_dotenv()
+
+API_KEY = os.getenv("API_KEY")   
 
 def verify_api_key(x_api_key: str = Header(...)):
     if x_api_key != API_KEY:
